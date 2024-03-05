a81c284f3df8bfb3c409136deee08aa72b43e3ef
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-03-05 14:53:17 +0530

    dynamic routing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './pages/Login';
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signin' element={<Login/>} />
+        
+      </Routes>
+    </>
+  )
+}
